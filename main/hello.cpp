71af437a92d48217d7aaeb5fc4bd3d418eaa2815
Varun chandrashekar <varun@Varuns-MacBook-Air.local>
2024-03-09 14:32:18 +0530

    Changes to Hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,6 +5,7 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
+  cout << "SRN: PES2UG21CS593\tNAME: VARUN C\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
